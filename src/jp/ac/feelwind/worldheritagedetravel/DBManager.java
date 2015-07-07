2d61eaf0711d268a0bceada1167d3e93d07de00d
student <student@PC633S03.asost.local>
2015-07-07 10:36:12 +0900

    db

--- src/jp/ac/feelwind/worldheritagedetravel/DBManager.java
+++ src/jp/ac/feelwind/worldheritagedetravel/DBManager.java
@@ -34,8 +34,36 @@ public class DBManager extends SQLiteOpenHelper {
 		try {
 			db.execSQL("CREATE TABLE IF NOT EXISTS "
 					+ "user(user_id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, total_point INTEGER)");
+
 			db.execSQL("CREATE TABLE IF NOT EXISTS "
-					+ "world_heritage(world_heritage_id INTEGER PRIMARY KEY AUTOINCREMENT, )");
+					+ "world_heritage(world_heritage_id INTEGER PRIMARY KEY AUTOINCREMENT, "
+					+ "world_heritage_name TEXT, explanation TEXT, world_heritage_image_id INTEGER, "
+					+ "area TEXT, location TEXT, climate TEXT, clothes TEXT)");
+
+			db.execSQL("CREATE TABLE IF NOT EXISTS "
+					+ "list(list_id INTEGER PRIMARY KEY AUTOINCREMENT, world_heritage_id INTEGER, "
+					+ "have_been_to TEXT, want_to_go TEXT)");
+
+			db.execSQL("CREATE TABLE IF NOT EXISTS "
+					+ "ranking(ranking_id INTEGER PRIMARY KEY AUTOINCREMENT, ranking INTEGER, "
+					+ "world_heritage_id INTEGER, explanation TEXT)");
+
+			db.execSQL("CREATE TABLE IF NOT EXISTS "
+					+ "world_heritage_image(world_heritage_image_id INTEGER PRIMARY KEY AUTOINCREMENT, "
+					+ "world_heritage_image BLOB)");
+
+			db.execSQL("CREATE TABLE IF NOT EXISTS "
+					+ "quiz(quiz_id INTEGER PRIMARY KEY AUTOINCREMENT, problem TEXT, selection_one TEXT, "
+					+ "selection_two TEXT, selection_three TEXT, selection_four TEXT, answer TEXT, "
+					+ "explanation TEXT, world_heritage_image_id INTEGER)");
+
+			db.execSQL("CREATE TABLE IF NOT EXISTS "
+					+ "character(character_id INTEGER PRIMARY KEY AUTOINCREMENT, character_image BLOB, "
+					+ "character_name TEXT)");
+
+			db.execSQL("CREATE TABLE IF NOT EXISTS "
+					+ "evolution_state(evolution_state_id INTEGER PRIMARY KEY AUTOINCREMENT, "
+					+ "character_id INTEGER)");
 
 			db.setTransactionSuccessful();
 		} catch (Exception e) {
@@ -53,6 +81,13 @@ public class DBManager extends SQLiteOpenHelper {
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 		// TODO 自動生成されたメソッド・スタブ
 		db.execSQL("DROP TABLE user");
+		db.execSQL("DROP TABLE world_heritage");
+		db.execSQL("DROP TABLE list");
+		db.execSQL("DROP TABLE ranking");
+		db.execSQL("DROP TABLE world_heritage_image");
+		db.execSQL("DROP TABLE quiz");
+		db.execSQL("DROP TABLE character");
+		db.execSQL("DROP TABLE evolution_state");
 		onCreate(db);
 	}
 
