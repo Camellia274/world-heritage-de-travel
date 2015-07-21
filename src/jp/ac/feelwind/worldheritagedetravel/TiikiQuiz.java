9e4e5af7ec9bbb2547610bcb1863965030f4b76e
student <student@PC633S03.asost.local>
2015-07-21 11:03:46 +0900

    ははははは

--- src/jp/ac/feelwind/worldheritagedetravel/TiikiQuiz.java
+++ src/jp/ac/feelwind/worldheritagedetravel/TiikiQuiz.java
@@ -1,14 +1,26 @@
 /**
- * 
+ *
  */
 package jp.ac.feelwind.worldheritagedetravel;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 /**
  * @author student
  *
  */
 public class TiikiQuiz extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO 自動生成されたメソッド・スタブ
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.areaselect);
+	}
 
+	@Override
+	protected void onResume() {
+		// TODO 自動生成されたメソッド・スタブ
+		super.onResume();
+	}
 }
