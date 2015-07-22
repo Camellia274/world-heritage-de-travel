ea6307e17ffc9512682b9ae16e36982cbbe66b24
student <student@PC633S03.asost.local>
2015-07-22 15:14:48 +0900

    まだ途中

--- src/jp/ac/feelwind/worldheritagedetravel/TiikiQuiz.java
+++ src/jp/ac/feelwind/worldheritagedetravel/TiikiQuiz.java
@@ -4,9 +4,11 @@
 package jp.ac.feelwind.worldheritagedetravel;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.ImageView;
 
 /**
  * @author student
@@ -24,12 +26,52 @@ public class TiikiQuiz extends Activity implements OnClickListener{
 	protected void onResume() {
 		// TODO 自動生成されたメソッド・スタブ
 		super.onResume();
+
+		ImageView europe1 = (ImageView)findViewById(R.id.imageViewEurope1);
+		europe1.setOnClickListener(this);
+		ImageView europe2 = (ImageView)findViewById(R.id.imageViewEurope2);
+		europe2.setOnClickListener(this);
+		ImageView asiaMiddleEast = (ImageView)findViewById(R.id.imageViewAsiaMiddleEast);
+		asiaMiddleEast.setOnClickListener(this);
+		ImageView africa = (ImageView)findViewById(R.id.imageViewAfrica);
+		africa.setOnClickListener(this);
+		ImageView oceania = (ImageView)findViewById(R.id.imageViewOceania);
+		oceania.setOnClickListener(this);
+		ImageView northAmericaSouthAmerica = (ImageView)findViewById(R.id.imageViewNorthAmericaSouthAmerica);
+		northAmericaSouthAmerica.setOnClickListener(this);
+
 	}
 
 	@Override
 	public void onClick(View v) {
 		// TODO 自動生成されたメソッド・スタブ
+		Intent intent = null;
+		switch (v.getId()) {
+			case R.id.imageViewEurope1:
+				intent = new Intent(TiikiQuiz.this,Europe1Quiz.class);
+				break;
 
+			case R.id.imageViewEurope2:
+				intent = new Intent(TiikiQuiz.this,Europe2Quiz.class);
+				break;
+
+			case R.id.imageViewAsiaMiddleEast:
+				intent = new Intent(TiikiQuiz.this,Europe1Quiz.class);
+				break;
+
+			case R.id.imageViewAfrica:
+				intent = new Intent(TiikiQuiz.this,Europe1Quiz.class);
+				break;
+
+			case R.id.imageViewOceania:
+				intent = new Intent(TiikiQuiz.this,Europe1Quiz.class);
+				break;
+
+			case R.id.imageViewNorthAmericaSouthAmerica:
+				intent = new Intent(TiikiQuiz.this,Europe1Quiz.class);
+				break;
+		}
+		startActivity(intent);
 	}
 
 }
